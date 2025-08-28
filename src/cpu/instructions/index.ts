a2fb470aff25c3c084c5cef695f6dff0e6add7c0
fwmakc <fwmakcpro@yandex.ru>
2025-08-28 17:15:34 +0300

    optimization instructions

---
+++ src/cpu/instructions/index.ts
@@ -0,0 +1,73 @@
+import ADD_I_VX from './ADD_I_VX.instruction';
+import ADD_VX_NN from './ADD_VX_NN.instruction';
+import ADD_VX_VY from './ADD_VX_VY.instruction';
+import AND_VX_VY from './AND_VX_VY.instruction';
+import CALL_ADDR from './CALL_ADDR.instruction';
+import CLS from './CLS.instruction';
+import DRW_VX_VY_N from './DRW_VX_VY_N.instruction';
+import DW from './DW.instruction';
+import JP_ADDR from './JP_ADDR.instruction';
+import JP_V0_ADDR from './JP_V0_ADDR.instruction';
+import LD_B_VX from './LD_B_VX.instruction';
+import LD_DT_VX from './LD_DT_VX.instruction';
+import LD_F_VX from './LD_F_VX.instruction';
+import LD_I_ADDR from './LD_I_ADDR.instruction';
+import LD_I_VX from './LD_I_VX.instruction';
+import LD_ST_VX from './LD_ST_VX.instruction';
+import LD_VX_DT from './LD_VX_DT.instruction';
+import LD_VX_I from './LD_VX_I.instruction';
+import LD_VX_N from './LD_VX_N.instruction';
+import LD_VX_NN from './LD_VX_NN.instruction';
+import LD_VX_VY from './LD_VX_VY.instruction';
+import OR_VX_VY from './OR_VX_VY.instruction';
+import RET from './RET.instruction';
+import RND_VX_NN from './RND_VX_NN.instruction';
+import SE_VX_NN from './SE_VX_NN.instruction';
+import SE_VX_VY from './SE_VX_VY.instruction';
+import SHL_VX_VY from './SHL_VX_VY.instruction';
+import SHR_VX_VY from './SHR_VX_VY.instruction';
+import SKNP_VX from './SKNP_VX.instruction';
+import SKP_VX from './SKP_VX.instruction';
+import SNE_VX_NN from './SNE_VX_NN.instruction';
+import SNE_VX_VY from './SNE_VX_VY.instruction';
+import SUBN_VX_VY from './SUBN_VX_VY.instruction';
+import SUB_VX_VY from './SUB_VX_VY.instruction';
+import XOR_VX_VY from './XOR_VX_VY.instruction';
+
+export {
+  ADD_I_VX,
+  ADD_VX_NN,
+  ADD_VX_VY,
+  AND_VX_VY,
+  CALL_ADDR,
+  CLS,
+  DRW_VX_VY_N,
+  DW,
+  JP_ADDR,
+  JP_V0_ADDR,
+  LD_B_VX,
+  LD_DT_VX,
+  LD_F_VX,
+  LD_I_ADDR,
+  LD_I_VX,
+  LD_ST_VX,
+  LD_VX_DT,
+  LD_VX_I,
+  LD_VX_N,
+  LD_VX_NN,
+  LD_VX_VY,
+  OR_VX_VY,
+  RET,
+  RND_VX_NN,
+  SE_VX_NN,
+  SE_VX_VY,
+  SHL_VX_VY,
+  SHR_VX_VY,
+  SKNP_VX,
+  SKP_VX,
+  SNE_VX_NN,
+  SNE_VX_VY,
+  SUBN_VX_VY,
+  SUB_VX_VY,
+  XOR_VX_VY,
+};
